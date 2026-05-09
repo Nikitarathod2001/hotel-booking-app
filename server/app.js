ef68a71cb25af6599ada32c888fdebbd636170da
Nikitarathod2001 <nikitarathod3601@gmail.com>
2026-05-09 19:50:29 +0530

    setup backend project structure with express and es6 modules

---
+++ server/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Hotel Booking App is running");
+});
+
+
+export default app;
